51c59298d2eb32586467193bb665dd3cfe6a5322
ekhaled <me.khaled@gmail.com>
2017-10-27 14:45:09 +0100

    eslint fixes

--- src/linearScale.js
+++ src/linearScale.js
@@ -1,8 +1,10 @@
 export default function linearScale(domain, range) {
-  var d0 = domain[0], r0 = range[0], multipler = (range[1] - r0) / (domain[1] - d0);
-  multipler = isFinite(multipler) ? multipler : 0;
+	const d0 = domain[0];
+	const r0 = range[0];
+	let multipler = (range[1] - r0) / (domain[1] - d0);
+	multipler = isFinite(multipler) ? multipler : 0;
 
-  return function (num) {
-    return r0 + ((num - d0) * multipler);
-  };
+	return function (num) {
+		return r0 + ((num - d0) * multipler);
+	};
 }
